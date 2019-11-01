d4f6ea54b1b7c4ef918e648a3c97277ff31b3bb3
Mercedes <mlombardi@udesa.edu.ar>
2019-11-01 17:05:41 -0300

    logo

---
+++ JavaScript/home.js
@@ -0,0 +1,7 @@
+var form= document.querySelector ("form");
+var usuario= document.querySelector ('input[name="usuario"]');
+var password= document.querySelector ('input[name="password"]');
+
+form.onsubmit = function(e){
+  e.
+}
