7df8bd9316852bf2126db066f0df22c9230517ac
Mercedes <mlombardi@udesa.edu.ar>
2019-11-06 17:02:33 -0300

    change

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -1,23 +1,27 @@
-//var url = 'https://api.themoviedb.org/3/tv/popular?api_key=0e65f11e4e58cb2a30446418b84e1eb4&language=en-US&page=1'
+/*var queryString = location.search;
+var searchParams = new URLSearchParams (queryString);
+var search = searchParams.get ("search")
+*/
+var url = 'https://api.themoviedb.org/3/tv/popular?api_key=0e65f11e4e58cb2a30446418b84e1eb4&language=en-US&page=1'
 
-fetch("https://api.themoviedb.org/3/tv/popular?api_key=0e65f11e4e58cb2a30446418b84e1eb4&language=en-US&page=1")
+fetch(url)
 .then(function(response){
   return response.json();
   console.log(response);
 })
+
 .then(function(information){
-  console.log(information);
-  var arrayDeSeries = datos.results
-  console.log (datos.results);
+  /* console.log(information) */;
+  var arrayDeSeries = information.results
+  console.log (information.results);
 
   for (var i = 0; i < arrayDeSeries.length; i++) {
-
+    var url = ""
     var titulo= arrayDeSeries[i].title
     var imagen= arrayDeSeries[i].poster_path
     var id= arrayDeSeries[i].id
-    elementoHTML = "<div class='uk-position-relative uk-visible-toggle uk-light'>"
+    elementoHTML += "<div class='uk-position-relative uk-visible-toggle uk-light'>"
     elementoHTML += "</div>"
-  document.querySelector(".populares").innerHTML+= "<li> <a href= detalle.html?idserie="+ id + "><figure class=imghvr-zoom-out-down style=background-color:black;><img src= " + url + imagen + ">" + "<figcaption class=masinfo style=background-color:black;> MÁS INFORMACIÓN </figcaption></figure>" + elementoHTML + "</a></li>"
 
   }
 
