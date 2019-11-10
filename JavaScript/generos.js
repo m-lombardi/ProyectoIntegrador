f2c80a0c2987a66275e2a85914fad210faeb542c
Mercedes <mlombardi@udesa.edu.ar>
2019-11-10 16:58:48 -0300

    var genero

---
+++ JavaScript/generos.js
@@ -0,0 +1,4 @@
+// nombre del generosvar idGenero = queryString.get("idgenero")
+var idGenero = queryString.get("idgenero")
+var nombreGen = queryString.get("nombregenero")
+document.querySelector(".nombredegenero").innerHTML= "<h2 class=generotitulo>" + nombreGen + "</h2>"
