98774828891d6c0f821047524c1bbc410031d773
Mercedes <mlombardi@udesa.edu.ar>
2019-11-22 15:06:10 -0300

    busqsimple detalles

--- JavaScript/detalle.js
+++ JavaScript/detalle.js
@@ -175,6 +175,34 @@ window.onload = function(){
         //window.localStorage.setItem ("nombre", formulariousuario.value);
       }
     })
+    fetch(`https://api.themoviedb.org/3/search/tv?api_key=0e65f11e4e58cb2a30446418b84e1eb4&language=en-US&query=`+ busqueda +`&page=1`)
+      .then(function(res) {
+        return res.json()
+      })
+      .then(function(myJson){
+        var posterURL= 'https://image.tmdb.org/t/p/original'
+        var elementoHTML= document.querySelector('.rated')
+        console.log(myJson);
+        var contenidoParaInsertar = ""
+          if (myJson.results.length == 0 ) {
+            //console.log("no hay resultados con " + busqueda);
+            var resultado = document.querySelector ('.resultado');
+            resultado.innerText = "No se han encontrado resultados para: " + busqueda
+          }else {
+
+
+        for (var i=0; i < myJson.results.length; i++){
+          //myJson.results[i]
+          // console.log ( myJson.results[i].name )
+          // console.log (posterURL+myJson.results[i].poster_path)
+          contenidoParaInsertar = '<li> <a href="detalle.html?id='+myJson.results[i].id+'">'
+          contenidoParaInsertar += '<img class="uk-height-small" src="'+ posterURL+ myJson.results[i].backdrop_path + '" alt="">'
+          contenidoParaInsertar += '<div class="uk-panel subtitulo2 "><h2>'+ myJson.results[i].name + '</h2></div>'
+          contenidoParaInsertar += '</a></li> '
+          elementoHTML.innerHTML += contenidoParaInsertar
+         }
+       }
+      })
 
 
 }
